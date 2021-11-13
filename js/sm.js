d82470b408285da1af9ef3ba8e36edc3b814746f
北山小虎 <asdfqs@163.com>
2021-11-13 22:18:44 +0800

    init & for quick start

---
+++ js/sm.js
@@ -0,0 +1,17 @@
+/**
+ * 我自己的 js 文件
+ */
+
+
+//  document.addEventListener('DOMContentLoaded', function(){
+
+//     const $container = $("#container");
+//     const url = "http://ops.q7link.com:8080"
+//      chrome.cookies.getAll({
+//         domain: url,
+//      }, cookie => {
+//         $container.text = cookie.map(c=>c.name + ":" + c.value + ";")
+//      })
+
+
+//  })
