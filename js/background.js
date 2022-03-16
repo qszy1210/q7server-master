00f67189fd7d958e403dd364e40d43603279db58
北山小虎 <asdfqs@163.com>
2022-03-16 16:20:01 +0800

    2022-03-16 add open url function

--- js/background.js
+++ js/background.js
@@ -4,3 +4,26 @@ chrome.commands.onCommand.addListener((command) => {
 		openUrlCurrentTab("http://localhost:3000/")
 	}
 });
+
+// 获取当前选项卡ID
+function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
+
+// 当前标签打开某个链接
+function openUrlCurrentTab(url)
+{
+	getCurrentTabId(tabId => {
+		chrome.tabs.update(tabId, {url: url});
+	})
+}
+
+// 新标签打开某个链接
+function openUrlNewTab(url)
+{
+	chrome.tabs.create({url: url});
+}
