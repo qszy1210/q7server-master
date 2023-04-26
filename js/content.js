99e32f39859dd287839b4ebeae9be881a868bbde
北山小虎 <asdfqs@163.com>
2023-04-26 20:39:01 +0800

    add auto expand gitlab protected branches setting

--- js/content.js
+++ js/content.js
@@ -6,3 +6,12 @@ chrome.extension.onMessage.addListener(
         }
     }
 );
+
+if (/.*gitlab.q7link.com.*repository\?page.*/.test(window.location.href)) {
+    $(document).ready(function () {
+        const $targetDom = $('#js-protected-branches-settings');
+        if ($targetDom && $targetDom.attr('class').indexOf('expanded') === -1) {
+            $targetDom.attr('class', $targetDom.attr('class') + '  expanded')
+        }
+    });
+}
